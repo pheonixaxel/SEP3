73fe0f23a0dada80da4195c9fbe496cedb74ac4b
Alex Cimpan <alex.cimpan0@yahoo.com>
2022-11-16 15:26:11 +0100

    Initial Tier 2 Commit

---
+++ SEP/SEP2_T2/src/main/java/com/example/sep2_t2/Sep2T2Application.java
@@ -0,0 +1,13 @@
+package com.example.sep2_t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep2T2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep2T2Application.class, args);
+	}
+
+}
